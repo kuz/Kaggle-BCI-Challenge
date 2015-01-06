00c04d2222199335eefc1a92a91c265a118f8ca1
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2015-01-06 16:53:45 +0200

    Overfitter needs the required packages to be preinstalled, will die otherwise

---
+++ Code/overfitter/install.r
@@ -0,0 +1,19 @@
+#
+#  List of packages to install for the Overfitter
+#
+
+install.packages('pROC')
+install.packages('caret')
+install.packages('ipred', 'plyr')
+install.packages('earth', 'mda')
+install.packages('logicFS')
+install.packages('arm')
+install.packages('ada')
+install.packages('mboost')
+install.packages('bst')
+install.packages('caTools')
+install.packages('party')
+install.packages('RWeka')
+install.packages('C50')
+install.packages('rpart')
+install.packages('party')
